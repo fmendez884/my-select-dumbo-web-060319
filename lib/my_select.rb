2024c9ada2e4c350bdf1ef7de2cd6ceb081e23ba
Francis Mendez <fmendez884@gmail.com>
2019-04-15 18:59:39 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,10 +2,10 @@ def my_select(collection)
   new = []
   i = 0
     while i < collection.length
-        if yield collection[i] == true 
-          new.push(yield collection[i])
-        end
-        i += 1
+      if yield collection[i] == true 
+        new.push(yield collection[i])
+      end
+      i += 1
     end 
     return new
 end
