f8afbb764a47f61cbbc8d11e007ff5021f033ee9
Francis Mendez <fmendez884@gmail.com>
2019-04-15 19:02:40 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -9,16 +9,3 @@ def my_select(collection)
     end 
     new
 end
-
-def my_select(collection)
- # your code here!
-  i = 0
-  selections = []
-  while collection.length > i
-    if yield(collection[i]) == true
-      selections << collection[i]
-    end
-    i += 1
-  end
-  selections
-end
