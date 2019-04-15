7592106ba2aae27cfd492c7b266b9f0853b88619
Francis Mendez <fmendez884@gmail.com>
2019-04-15 19:01:23 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,11 +1,24 @@
+# def my_select(collection)
+#   new = []
+#   i = 0
+#     while i < collection.length
+#       if yield(collection[i) == true 
+#         new << yield collection[i]
+#       end
+#       i += 1
+#     end 
+#     new
+# end
+
 def my_select(collection)
-  new = []
+ # your code here!
   i = 0
-    while i < collection.length
-      if yield(collection[i) == true 
-        new << yield collection[i]
-      end
-      i += 1
-    end 
-    new
+  selections = []
+  while collection.length > i
+    if yield(collection[i]) == true
+      selections << collection[i]
+    end
+    i += 1
+  end
+  selections
 end
